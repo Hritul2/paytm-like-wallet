7fe5300c4a45c6637870df107e87b71014f9996a
Hritul <hritul.srivastava@gmail.com>
2024-10-09 11:30:07 +0530

    redirect according the use session on Home Page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
-const page = () => {
-  return <div>Hi</div>;
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../lib/auth";
+// TODO: Add the Landing Page
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+  redirect("/api/auth/signin");
 };
 
 export default page;
